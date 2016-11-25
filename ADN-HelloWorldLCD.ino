dedc822069c87f3adb1e6adb9c3edede731d75a8
levalleuxludo <levalleux_ludo@hotmail.com>
2016-11-25 23:54:25 +0100

    HelloWorld avec compteur de millisecondes

--- ADN-HelloWorldLCD.ino
+++ ADN-HelloWorldLCD.ino
@@ -1,5 +1,9 @@
 #include <LiquidCrystal.h>
 
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
+ 
+int compteur = 0;
+
 void setup() {
   // put your setup code here, to run once:
   lcd.begin(16, 2);
@@ -9,6 +13,9 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+ // set the cursor to column 0, line 1
+  // (note: line 1 is the second row, since counting begins with 0):
+  lcd.setCursor(0, 1);
+  // print the number of seconds since reset:
+  lcd.print(millis());
 }
